43b73f067b64a9999a063f06b13ff785e9409493
eitan-berenfeld <eitandavidberenfeld@gmail.com>
2025-12-27 22:48:14 -0500

    Virtual environment setup and dependencies

--- analysis/causal_project.py
+++ analysis/causal_project.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-file_path = "my_project/data/Store_Dataset.csv"
+file_path = "causal_retail_analysis/data/Store_Dataset.csv"
 store_df = pd.read_csv(file_path)
 
-file_path = "my_project/data/store_location_dataset.csv"
+file_path = "causal_retail_analysis/data/store_location_dataset.csv"
 store_loc_df = pd.read_csv(file_path)
 store_loc_df['store_id'] = store_loc_df['index']
 
 from datasets import load_dataset
 
 fresh_ds = load_dataset("Dingdong-Inc/FreshRetailNet-50K")
-df = fresh_df['train'].to_pandas()
+df = fresh_ds['train'].to_pandas()
 df.head()
 df.columns
 
